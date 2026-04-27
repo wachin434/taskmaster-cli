57e981aafe786e95b7810ec9a132f09cfaca9563
Vnderr <=>
2026-04-27 15:48:30 -0400

    feat: add task command

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 const { readTasks, writeTasks } = require('./tasks');
 const [,, cmd] = process.argv;
 
+
 if (cmd === 'list') {
     const tasks = readTasks();
     if (tasks.length === 0) {
@@ -11,4 +12,17 @@ if (cmd === 'list') {
             console.log('\${icon} [\${i+1}] \${t.title}');
         });
     }
+}
+
+const title = process.argv.slice(3).join(' ');
+
+if(cmd === 'add') {
+    if(!title) {
+        console.log('Uso: node index.js add <titulo>');
+        process.exit(1);
+    }
+    const tasks = readTasks();
+    tasks.push({ id: Date.now(), title, done: false });
+    writeTasks(tasks);
+    console.log('✓ Tarea "\${title}" agregar');
 }
